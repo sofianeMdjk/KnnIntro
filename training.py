80d8176c6338cafcab9ddab19e7fee6f63ce2bde
sofianeMdjk <sofiane.mdjk@gmail.com>
2018-06-05 00:17:35 +0200

    implemented a basic example of knn training

--- training.py
+++ training.py
@@ -1,9 +1,37 @@
 import pandas as pd
+import preporcessing as pre
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
-## Specifying column names
+#Specifying column names
 col_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-iris_dataset = pd.read_csv(url, header=None, names=col_names)
+
+#loading dataset (internet connexion required)
+dataset = pd.read_csv(url, header=None, names=col_names)
+
+# mapping classes to integer values
+
+classes = {'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2}
+dataset['species_num'] = [classes[i] for i in dataset.species]
+
+#getting inputs and target
+inputs = dataset.drop(['species', 'species_num'], axis=1)
+target = dataset.species_num
+
+#normalizing inputs
+input = pre.dataNormalization(inputs)
+
+#splitting data into train and test
+trainInput,testInput,trainTarget,testTarget = train_test_split(input,target,test_size=0.25)
+
+#training clissifier
+classifier = KNeighborsClassifier(n_neighbors=4)
+## Fit the model on the training data.
+classifier.fit(trainInput, trainTarget)
+## See how the model performs on the test data.
+accuracy = classifier.score(testInput, testTarget)
+print(accuracy)
 
 
